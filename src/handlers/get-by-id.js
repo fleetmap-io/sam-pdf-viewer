e087ffc66d9f56a1e1e1ae7279bd641cd57c01c6
jcardus <asklocation.net@gmail.com>
2022-08-03 20:34:32 +0100

    queryStringParameters

--- src/handlers/get-by-id.js
+++ src/handlers/get-by-id.js
@@ -1,5 +1,5 @@
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
 
 /**
  * A simple example includes a HTTP get method to get one item by id from a DynamoDB table.
@@ -11,46 +11,31 @@ exports.getByIdHandler = async (event) => {
   // All log statements are written to CloudWatch
   console.info('received:', event);
   const id = event.pathParameters.id;
+
   const params = {
     Bucket: 'manager-mobile-s3-pdfs',
     Key: id,
   }
 
-  const o = await s3.getObject(params).promise()
+  if (event.queryStringParameters && event.queryStringParameters.raw) {
+    const o = await s3.getObject(params).promise()
+    const response = {
+      statusCode: 200,
+      headers: {'content-type': 'application/pdf', 'Access-Control-Allow-Origin': '*'},
+      body: o.Body
+    }
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response
+  }
+
   const response = {
     statusCode: 200,
     headers: {'content-type': 'text/html'},
-    body: `<html lang="pt">
-                <iframe id="theFrame" style="padding: 0; border:0; width:100%; height: 100%" src="${o.Body}" />
-           </html>
-          <script>
-          document.getElementById("theFrame").contentWindow.onload = function() {
-              this.document.getElementsByTagName("img")[0].style.width="100%";
-              this.document.getElementsByTagName("img")[0].style.height="100%";
-          };
-          </script>`
-  }
-
-  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-  return response
-}
-exports.getPdfHandler = async (event) => {
-  if (event.httpMethod !== 'GET') {
-    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
-  }
-  // All log statements are written to CloudWatch
-  console.info('received:', event);
-  const id = event.pathParameters.id;
-  const params = {
-    Bucket: 'manager-mobile-s3-pdfs',
-    Key: id,
-  }
-
-  const o = await s3.getObject(params).promise()
-  const response = {
-    statusCode: 200,
-    headers: {'content-type': 'application/pdf'},
-    body: o.Body
+    body: `<iframe
+              id="ifamePdf"              
+              src="https://docs.google.com/viewerng/viewer?url=https://tqdeegmk8f.execute-api.us-east-1.amazonaws.com/Prod/${id}%3fraw=true&embedded=true"
+              height="600"
+            ></iframe>`
   }
 
   console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
