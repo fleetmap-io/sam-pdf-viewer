182ceccd3c7b641dc0b420d831fd2c9d4cd90e31
jcardus <asklocation.net@gmail.com>
2022-08-03 19:49:25 +0100

    remove get-by-id add getPdfFunction

--- src/handlers/get-by-id.js
+++ src/handlers/get-by-id.js
@@ -20,9 +20,39 @@ exports.getByIdHandler = async (event) => {
   const response = {
     statusCode: 200,
     headers: {'content-type': 'text/html'},
-    body: `<html lang="pt"><iframe style="padding: 0; border:0; width:100vw; height: 100vh" src="${o.Body}" /></html>`
+    body: `<html lang="pt">
+                <iframe id="theFrame" style="padding: 0; border:0; width:100%; height: 100%" src="${o.Body}" />
+           </html>
+          <script>
+          document.getElementById("theFrame").contentWindow.onload = function() {
+              this.document.getElementsByTagName("img")[0].style.width="100%";
+              this.document.getElementsByTagName("img")[0].style.height="100%";
+          };
+          </script>`
   }
 
   console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-  return response;
+  return response
+}
+exports.getPdfHandler = async (event) => {
+  if (event.httpMethod !== 'GET') {
+    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+  }
+  // All log statements are written to CloudWatch
+  console.info('received:', event);
+  const id = event.pathParameters.id;
+  const params = {
+    Bucket: 'manager-mobile-s3-pdfs',
+    Key: id,
+  }
+
+  const o = await s3.getObject(params).promise()
+  const response = {
+    statusCode: 200,
+    headers: {'content-type': 'application/pdf'},
+    body: o.Body
+  }
+
+  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  return response
 }
