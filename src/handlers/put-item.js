ce438166d7ed5f7b8078d75687051bfbcb15fef2
jcardus <asklocation.net@gmail.com>
2022-08-03 18:39:04 +0100

    it's working

--- src/handlers/put-item.js
+++ src/handlers/put-item.js
@@ -22,21 +22,19 @@ exports.putItemHandler = async (event) => {
     const id = body.id;
     const name = body.name;
 
-    // Creates a new item, or replaces an old item with a new item
-    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
-    var params = {
-        TableName : tableName,
-        Item: { id : id, name: name }
+    const params = {
+        TableName: tableName,
+        Item: {id: id, name: name}
     };
 
     await docClient.put(params).promise();
 
     const response = {
         statusCode: 200,
-        body: JSON.stringify(body)
-    };
+        headers: {'Access-Control-Allow-Origin': '*'}
+    }
 
     // All log statements are written to CloudWatch
-    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
-    return response;
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode}`)
+    return response
 };
