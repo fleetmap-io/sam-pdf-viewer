fe24b0fee54ee2c832cd853c2df8f853b5336e6b
jcardus <asklocation.net@gmail.com>
2023-09-10 14:40:28 +0100

    add fleetmap-reports

--- src/handlers/put-item.js
+++ src/handlers/put-item.js
@@ -1,18 +1,16 @@
 const AWS = require('aws-sdk');
+const Reports = require("fleetmap-reports");
 const s3 = new AWS.S3();
 
 /**
  * A simple example includes a HTTP post method to add one item to a DynamoDB table.
  */
 exports.putItemHandler = async (event) => {
-    if (event.httpMethod !== 'POST') {
-        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
-    }
     console.info('received:', event);
-
-    console.log('datauri', event.body)
-
-
+    if (event.httpMethod !== 'POST') {
+        const pdf = await new Reports()[event.queryStringParameters.reportType + 'ReportToPDF'](event.body.userData, event.body.reportData)
+        event.body = pdf.output('datauristring')
+    }
     const params = {
         Bucket: 'manager-mobile-s3-pdfs',
         Key: event.pathParameters.id,
@@ -20,13 +18,10 @@ exports.putItemHandler = async (event) => {
         ContentType: 'application/pdf'
     };
     await s3.putObject(params).promise()
-
     const response = {
         statusCode: 200,
         headers: {'Access-Control-Allow-Origin': '*'}
     }
-
-    // All log statements are written to CloudWatch
     console.info(`response from: ${event.path} statusCode: ${response.statusCode}`)
     return response
 };
